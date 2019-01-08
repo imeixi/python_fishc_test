b6b0ec09602c8a8234d46e967de964034b4880ba
imeixi <z.ah_r@163.com>
2019-01-09 00:11:04 +0800

    添加第二课练习

---
+++ 002/test_1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+name = input("please input your name:")
+print("hello", name)
---
+++ 002/test_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+text = input("please input one number between 0 to 100:")
+num = int(text)
+if 0 < num < 100:
+    print("you are a good man")
+else:
+    print("Hey, man ,you give a wrong num")
